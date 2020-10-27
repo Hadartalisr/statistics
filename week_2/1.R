a9b15a62e30594a90c3b9daca6eabaead9e947ed
HadarTal <hadar.tal.isr@gmail.com>
2020-10-27 17:47:16 +0200

    working on 2

--- week_2/1.R
+++ week_2/1.R
@@ -1,12 +1,11 @@
 library(sqldf)
 
 
-
 #q.1
 # according to the question in the forum, 
 # it is possible to use advanced R functions
 density.kernel <- function(x, h){
-  plot(density(math,kernel = c("cosine"),window = kernel, bw = h))
+  plot(density(x,kernel = c("cosine"),window = kernel, bw = h))
 }
 
 
@@ -22,4 +21,22 @@ for (h in c(0.5,1,1.5,2,2.5)){
 # From the following graphs we can conclude that the best h* is 1.5
 # this h let us balance between the need to show as much details a possible
 # and yet to present the main information which we can achieve from the data.
+
+
+#q.3
+# The distribution has 3 modals - at about 50, 75 and 90.
+
+
+#q.4
+
+a_math = sqldf('Select math from df a where a.school = "A"')$math
+plot(density(a_math, kernel = c("cosine"),window = kernel, bw = 1.5),
+     xlim=c(0,100), ylim=c(0,0.15))
+b_math = sqldf('Select math from df a where a.school = "B"')$math
+lines(density(b_math, kernel = c("cosine"),window = kernel, bw = 1.5),add=T)
+c_math = sqldf('Select math from df a where a.school = "C"')$math
+lines(density(c_math, kernel = c("cosine"),window = kernel, bw = 1.5),add=T)
+
+
+
 
