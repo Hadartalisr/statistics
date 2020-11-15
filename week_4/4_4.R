1b5110b36a6fd3730316eeea650e342d2928ca46
HadarTal <hadar.tal.isr@gmail.com>
2020-11-15 18:21:32 +0200

    q4

--- week_4/4_4.R
+++ week_4/4_4.R
@@ -1,5 +1,7 @@
 #q.4
 
+par(mfrow=c(1,1))
+
 my_var <- function(v){
   return (mean((v)^2) - (mean(v))^2)
 }
@@ -31,12 +33,11 @@ get_random_x2 <- function(){
 x2 = get_random_x2()
 # mean of x2 (2 cube results) equals 2 mean of x (Expectation is linear)
 # therefore, mean(x2) = mean(x+x) = 2 mean(x) = 2*3.5 = 7
+mean_x2 = 7
 # the cubes are i.i.d therefore - 
 # var(y) = var(x1+x2) = var(x1) + var(x2) = 2*var(x)
 sd_x2 = sqrt(2*var_x)
 sd_x2
-mean_x2 = mean(x2)
-mean_x2
 
 
 #q.2
@@ -50,15 +51,41 @@ mean_x2_vec
 sd_x2_vec = my_sd(x2_vec)
 sd_x2_vec
 #calculate the diff :
-mean_x2_vec - mean_x2
+mean_x2_vec - 7
 sd_x2_vec - sd_x2
 # thus, the results were close to the expected theory values.
 
 
 #q.3
+x <- seq(-10, 20,length=1000)
+y <- dnorm(x, mean=mean_x2, sd=sd_x2)
+lines(x, y, type="l", lwd=1)
+# yes, we can say that x2 distribute approximately noraml
 
 
+#q.4
+ 
 
+n_vec = c()
+mean_vec = c()
+sd_vec = c()
 
+par(mfrow=c(2,2))
+for(n in seq(200,2000,600)){
+  plot(density(x2_vec), main = n, xlab = "result")
+  
+  n_vec = c(n_vec,n)
+  x3_vec = c()
+  for(i in seq(1,n)){
+    x3_vec = c(x3_vec, get_random_x2())
+  }
+  lines(density(x3_vec), xlab = "", ylab = "")
+
+  mean_x3_vec = mean(x3_vec)
+  mean_vec = c(mean_vec, mean_x3_vec)
+  
+  sd_x3_vec = my_sd(x3_vec)
+  sd_vec = c(sd_vec, sd_x3_vec)
+}
 
 
